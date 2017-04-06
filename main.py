3ea9066c91993458811f7798fe82fcfe3f81a0e5
Chester <chesta@kekt.us>
2017-04-06 22:51:58 +0200

    Made x position translate to rotation of the stepper

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import serial
 from random import randint
+import tracking
 
 arduino = serial.Serial("\\\\.\\COM3")
 print(arduino.readline())
 print(arduino.writeTimeout)
 
-while True:
-    deg = (str(randint(1, 359)) + "\n").encode('UTF-8')
-    #(input("Enter desired degrees: ") + "\n").encode('UTF-8')
+
+def onPosition(pos):
+    deg = int(pos[0] / 1.6)
     print(deg)
-    arduino.write(deg)
+    arduino.write((str(deg) + '\r').encode('UTF-8'))
     print(arduino.readline())
+
+tracking.track(onPosition)
+
+#while True:
+#    deg = (str(randint(1, 359)) + "\n").encode('UTF-8')
+#    #(input("Enter desired degrees: ") + "\n").encode('UTF-8')
+#    print(deg)
+#    arduino.write(deg)
+#    print(arduino.readline())
 
