60816545ec3c7533822ec50bbbdb908f0e98ca15
Chester <chesta@kekt.us>
2017-04-10 09:11:21 +0200

    Refactored python files to be object-oriented

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import serial
-from random import randint
-import tracking
+from tracking import Tracking
 
 
-# arduino = serial.Serial("\\\\.\\COM3")
-# print(arduino.readline())
-# print(arduino.writeTimeout)
+class CamBalance:
+    lastStep = 0
+    tracking = Tracking()
 
-def onNewPos(pos, history):
-    print(pos)
+    def sendStep(self, step):
+        if step is not self.lastStep:
+            print(step)
+            self.arduino.write((str(step) + "\n").encode('UTF-8'))
+            print(self.arduino.readline())
+            self.lastStep = step
 
-tracking.track(onNewPos)
+    def onNewPos(self, pos, history):
+        print(pos)
 
-# while True:
-#     deg = (str(randint(1, 359)) + "\n").encode('UTF-8')
-#     #(input("Enter desired degrees: ") + "\n").encode('UTF-8')
-#     print(deg)
-#     arduino.write(deg)
-#     print(arduino.readline())
+    def start(self, port="\\\\.\\COM3"):
+        self.arduino = serial.Serial(port)
+        print(self.arduino.readline())
+        self.tracking.start(self.onNewPos)
 
+
+balance = CamBalance()
+balance.start()
--- tracking.py
+++ tracking.py
@@ -6,68 +6,73 @@ from collections import deque
 from rangeDetector import selectRange
 
 
-def track(onNewCoordinate = None):
-    # start webcam
-    camera = cv2.VideoCapture(0)
-
+class Tracking:
     lowerColor = (169, 140, 131)
     upperColor = (180, 255, 255)
     pointHistory = deque(maxlen=30)
 
-    while True:
-        # get current webcam frame
-        (grabbed, frame) = camera.read()
+    def start(self, onNewCoordinate = None):
+        # start webcam
+        camera = cv2.VideoCapture(0)
 
-        # resize and convert frame to hsv
-        frame = imutils.resize(frame, width=600)
-        copy = frame.copy()
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lowerColor = (169, 140, 131)
+        upperColor = (180, 255, 255)
+        pointHistory = deque(maxlen=30)
 
-        # generate mask
-        mask = cv2.inRange(hsv, lowerColor, upperColor)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
+        while True:
+            # get current webcam frame
+            (grabbed, frame) = camera.read()
 
-        # find contours of the ball ([-2] for compatibility)
-        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
+            # resize and convert frame to hsv
+            frame = imutils.resize(frame, width=600)
+            copy = frame.copy()
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        if len(contours) > 0:
-            # find largest contour
-            c = max(contours, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            # generate mask
+            mask = cv2.inRange(hsv, lowerColor, upperColor)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
 
-            if onNewCoordinate is not None:
-                onNewCoordinate(center, pointHistory)
+            # find contours of the ball ([-2] for compatibility)
+            contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+            center = None
 
-            # TODO: find minimum radius
-            if radius > 5:
-                # draw outline and center of tracked ball
-                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+            if len(contours) > 0:
+                # find largest contour
+                c = max(contours, key=cv2.contourArea)
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                M = cv2.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-        pointHistory.appendleft(center)
+                if onNewCoordinate is not None:
+                    onNewCoordinate(center, pointHistory)
 
-        # draw history
-        for i in range(1, len(pointHistory)):
-            if pointHistory[i - 1] is None or pointHistory[i] is None:
-                continue
+                # TODO: find minimum radius
+                if radius > 5:
+                    # draw outline and center of tracked ball
+                    cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+                    cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
-            thickness = int(np.sqrt(pointHistory.maxlen / float(i + 1)) * 2.5)
-            cv2.line(frame, pointHistory[i - 1], pointHistory[i], (0, 0, 255), thickness)
+            pointHistory.appendleft(center)
 
-        cv2.imshow("Frame", frame)
+            # draw history
+            for i in range(1, len(pointHistory)):
+                if pointHistory[i - 1] is None or pointHistory[i] is None:
+                    continue
 
-        # exit on 'q'
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
-        elif key == ord('a'):
-            (lowerColor, upperColor) = selectRange(copy, values=(lowerColor, upperColor))
-            print("lowerColor: " + str(lowerColor))
-            print("upperColor: " + str(upperColor))
+                thickness = int(np.sqrt(pointHistory.maxlen / float(i + 1)) * 2.5)
+                cv2.line(frame, pointHistory[i - 1], pointHistory[i], (0, 0, 255), thickness)
 
-    camera.release()
-    cv2.destroyAllWindows()
+            cv2.imshow("Frame", frame)
+
+            # exit on 'q'
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+            elif key == ord('a'):
+                (lowerColor, upperColor) = selectRange(copy, values=(lowerColor, upperColor))
+                print("lowerColor: " + str(lowerColor))
+                print("upperColor: " + str(upperColor))
+
+        camera.release()
+        cv2.destroyAllWindows()
