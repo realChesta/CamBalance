6df34c6fbe08fdc979229ff3b3fa454ca9915663
OverlordMorgoth <nikola@kekt.us>
2017-05-01 23:49:59 +0200

    First few lines* :)

--- mati.py
+++ mati.py
@@ -5,4 +5,4 @@ import scipy as sp
 def math(x0, y0, x1, y1, dt, td):
     V0 = np.sqrt(np.square(x0-x1)+np.square(y0-y1))/dt
     X0 = np.sqrt(np.square(x1)+np.square(y1))
-    a0 = np.arctan(x)
+    a0 = np.arctan(y1/x1)
