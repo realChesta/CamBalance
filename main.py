ea5fb14397853b66c682b0856626b6ce0cf31327
Chester <chesta@kekt.us>
2017-05-17 14:13:40 +0200

    Implemented simple angle control

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import serial
+from math import ceil
 from tracking import Tracking
 
 
@@ -7,19 +8,23 @@ class CamBalance:
     tracking = Tracking()
 
     def sendStep(self, step):
-        if step is not self.lastStep:
+        if step != self.lastStep:
             print(step)
             self.arduino.write((str(step) + "\n").encode('UTF-8'))
             print(self.arduino.readline())
             self.lastStep = step
 
+    def sendAngle(self, angle):
+        self.sendStep(ceil(angle / 7.5))
+
     def onNewPos(self, pos, history):
-        print(pos)
+        self.sendAngle(pos[0] / 16)
 
     def start(self, port="\\\\.\\COM3"):
         self.arduino = serial.Serial(port)
         print(self.arduino.readline())
         self.tracking.start(self.onNewPos)
+        self.sendStep(12) #set angle to neutral pos
 
 
 balance = CamBalance()
